bab3522b5cb79c71cdd519e204044fddf48f8fc4
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-07-09 03:51:22 +0300

    Add about content

--- src/about.js
+++ src/about.js
@@ -9,12 +9,19 @@ const aboutTab = (() => {
     // assign classes elements
     about.classList.add("about");
     aboutHeading.classList.add("aboutHeading");
-    aboutContent.classList.add("aboutContent");
+    aboutContent.classList.add("aboutContent", "content");
 
     // organise and content to elements// about
     aboutHeading.innerHTML = "ABOUT";
+    aboutContent.innerHTML = `The Michi story is a wild ride to say the least. 
+                              When we started in 2012 with a food truck, we couldn't 
+                              have imagined being named the best Ramen in Austin by our 
+                              fans two short years later. In a town long dominated by 
+                              enchiladas and burgers, we've loved introducing Austinites 
+                              and Texans to our favorite food. `
 
     about.appendChild(aboutHeading);
+    about.appendChild(aboutContent);
 
     return about;
 });
